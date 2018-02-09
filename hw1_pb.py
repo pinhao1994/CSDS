a5b31d58c75d9e1f3659d62a70e8d27a1ca0024f
Ubuntu <ubuntu@ip-172-31-41-191.us-east-2.compute.internal>
2018-02-09 07:06:15 +0000

    upload csds hw1 pb & sql

---
+++ hw1_pb.py
@@ -0,0 +1,30 @@
+import twitter_pb2 as pb2
+#import sys
+
+# main code
+twts = pb2.Tweets()
+with open("twitter.pb", "rb") as file:
+	twts.ParseFromString(file.read())
+	file.close()
+
+# 1. Find the number of deleted messages in the dataset
+# 2. Find the number of tweets that are replies to another tweet
+# 3. Find the five user IDs (field name: uid) that have tweeted the most
+count_del = 0
+count_re = 0
+count_uid = dict()
+for twt in twts.tweets:
+	if twt.is_delete: count_del += 1
+	if twt.HasField("insert") and twt.insert.reply_to: count_re += 1
+	if twt.HasField("insert"):
+		if str(twt.insert.uid) not in count_uid:
+			count_uid[str(twt.insert.uid)] = 1
+		else:
+			count_uid[str(twt.insert.uid)] += 1
+
+count_uid = sorted(count_uid.items(), key=lambda x:x[1], reverse=True)
+count_uid = [str(uid) for uid, count in count_uid[0:5]]
+
+print("Number of Deleted Messages: %d" % count_del)
+print("Number of tweets that are replies to another tweet: %d" % count_re)
+print("UID of the top 5 users who have tweeted the most: \n%s" % "\n".join(count_uid))
---
+++ hw1_sql.sql
@@ -0,0 +1,9 @@
+-- 1. the number of deleted messages in the dataset
+select count('is_delete') from tweets where is_delete = 1;
+
+-- 2. the number of tweets that are replies to another tweet 
+select count('reply_to') from tweets where reply_to != 0;
+
+-- 3. the five user uid that have tweeted the most
+select uid from tweets where text != "" group by uid order by count(uid) DESC limit 5;
+

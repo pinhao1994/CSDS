736b1c16d8caf1fdfc652841656e489309bb7fd1
Ubuntu <ubuntu@ip-172-31-41-191.us-east-2.compute.internal>
2018-02-10 02:00:19 +0000

    update HW1

--- HW1/hw1_sql.sql
+++ HW1/hw1_sql.sql
@@ -2,7 +2,6 @@
 select count('is_delete') from tweets where is_delete = 1;
 
 -- 2. the number of tweets that are replies to another tweet 
--- select count('reply_to') from tweets where reply_to != 0;
 select count(distinct id) from tweets where reply_to != 0;
 
 -- 3. the five user uid that have tweeted the most
